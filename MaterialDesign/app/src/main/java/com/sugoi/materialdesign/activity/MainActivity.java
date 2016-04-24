9660f9cc54960d9b2a118ebba02c1434e4713dff
Rahman Adianto <rahman.adianto@gmail.com>
2016-04-24 11:29:31 +0700

    Add navigation drawer

--- MaterialDesign/app/src/main/java/com/sugoi/materialdesign/activity/MainActivity.java
+++ MaterialDesign/app/src/main/java/com/sugoi/materialdesign/activity/MainActivity.java
@@ -1,14 +1,21 @@
-package com.sugoi.materialdesign;
+package com.sugoi.materialdesign.activity;
 
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.sugoi.materialdesign.R;
+import com.sugoi.materialdesign.fragment.FragmentDrawer;
+
+public class MainActivity extends AppCompatActivity implements FragmentDrawer.FragmentDrawerListener {
 
     private Toolbar toolbar;
+    private FragmentDrawer fragmentDrawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,11 +23,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         toolbar = (Toolbar) findViewById(R.id.toolbar);
-
         setSupportActionBar(toolbar);
         if (getSupportActionBar() != null) {
             getSupportActionBar().setDisplayShowHomeEnabled(true);
         }
+
+        FragmentManager fm = getSupportFragmentManager();
+        fragmentDrawer = (FragmentDrawer) fm.findFragmentById(R.id.fragment_navigation_drawer);
+        fragmentDrawer.setUp(R.id.fragment_navigation_drawer, (DrawerLayout) findViewById(R.id.drawer_layout), toolbar);
+        fragmentDrawer.setDrawerListener(this);
     }
 
     @Override
@@ -38,5 +49,10 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onDrawerItemSelected(View view, int position) {
+
     }
 }
